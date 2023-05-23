913a2c7f9631c6b72e05108a59b14a272e460f44
devCKVargas <50130192+devCKVargas@users.noreply.github.com>
2023-05-23 20:49:53 +0800

    Day 16: Afternoon

--- script/script.js
+++ script/script.js
@@ -1,89 +1,79 @@
 `use strict`;
 
-//? //** CHANGE DAY HERE */
-//? //** CHANGE DAY HERE */
-//? //** CHANGE DAY HERE */
-//TODO turn this 👇 into a function
-const currentDay = 16;
-//TODO turn this ☝ into a function
-//? //** CHANGE DAY HERE */
-//? //** CHANGE DAY HERE */
-//? //** CHANGE DAY HERE */
+//? FOR NOW CHANGE DAY HERE or call dayCounter() */
+let currentDay = 16;
 
-//** Selecting elements */
+//* Selecting elements */
 const leftBG = document.querySelector(`.left`);
 const dayNumber = document.querySelector(`.day`);
 const leftContent = document.querySelector(`.left--content`);
 const hashTag = document.querySelectorAll(`.hashtag`);
-const codeTime = document.querySelector(`.code--time`);
+const codeTime = document.getElementById(`code--time`);
 const colored = document.querySelectorAll(`.colored`);
 const twitterHandle = document.querySelector(`.twitter--handle`);
 const rightBG = document.querySelector(`.right`);
 const listItems = document.querySelectorAll(`.list--items`);
 const listTitle = document.querySelectorAll(`.list--title`);
-// const darkModeBtn = document.querySelector(`#darkModeBtn`);
 const darkModeBtn = document.querySelector(`#hide-checkbox`); //* checkbox
 
-//** Not working */
+//* Not working */
 
-//** Initial values */
+//* Initial values */
 const timeOfDay = new Date().getHours();
 dayNumber.innerText = `DAY ${currentDay}`;
+
+//? Fix day counter - Done on: 05/23/2023
+const dayCounter = () => {
+	const dateStart = new Date(2023, 04, 06);
+	const dateNow = new Date() / (1000 * 3600 * 24);
+	const dateComplete = new Date(2023, 07, 14);
+	const remainingDays =
+		(dateComplete.getTime() - dateStart.getTime()) / (1000 * 3600 * 24);
+	const spentDays = Math.trunc((dateNow - remainingDays) / 1000) - 1;
+	console.log(spentDays);
+
+	dayNumber.innerText = `DAY ${spentDays}`;
+};
+// dayCounter(); //TODO turn this on once first day 100 is completed
+
 //TODO animate auto mode (when time is 1PM)
 const autoModeAnimation = {
 	duration: 200,
 	iterations: 1,
 };
 
-//** DARK MODE STARTS HERE */
+//* DARK MODE STARTS HERE */
 const darkMode = () => {
 	document.body.classList.toggle(`body--dark`);
 	leftBG.classList.toggle(`left--dark`);
-	dayNumber.classList.toggle(`colored--dark`);
 	twitterHandle.classList.toggle(`twitter--handle--dark`);
 	rightBG.classList.toggle(`right--dark`);
-	//! Hash Tags color */
+	//? colored elements */
+	for (const items of colored) items.classList.toggle(`colored--dark`);
+	//? Hash Tags color */
 	const hashTagToggler = () => {
-		for (let i = 0; i < hashTag.length; i++) {
-			hashTag[i].classList.toggle(`hashtag--dark`);
-		}
+		for (const items of hashTag) items.classList.toggle(`hashtag--dark`);
 	};
-	//! List Titles color */
+	//? List Titles color */
 	const darkListTitleToggler = () => {
-		for (let i = 0; i < listTitle.length; i++) {
-			listTitle[i].classList.toggle(`list--title--dark`);
-		}
+		for (const items of listTitle) items.classList.toggle(`list--title--dark`);
 	};
-	//! List Items color */
+	//? List Items color */
 	const darkListItemsToggler = () => {
-		for (let i = 0; i < listItems.length; i++) {
-			listItems[i].classList.toggle(`list--items--dark`);
-		}
+		for (const items of listItems) items.classList.toggle(`list--items--dark`);
 	};
-	//! Interchange hashtag text: Morning 🔁 Afternoon */
-	const replaceString = () => {
-		if (codeTime.textContent.includes(`Morning`)) {
-			codeTime.textContent = codeTime.textContent.replace(
-				`Morning`,
-				`Afternoon`
-			);
-		} else {
-			codeTime.textContent = codeTime.textContent.replace(
-				`Afternoon`,
-				`Morning`
-			);
-		}
+	//? Interchange hashtag text: Morning 🔁 Afternoon */
+	const morningAfternoonCodeSwap = () => {
+		codeTime.text.includes(`Morning`)
+			? (codeTime.text = `#AfternoonCode`)
+			: (codeTime.text = `#MorningCode`);
 	};
-	//! darkModeBtn flipper */\
-
-	replaceString();
+	morningAfternoonCodeSwap();
 	hashTagToggler();
 	darkListTitleToggler();
 	darkListItemsToggler();
-	colored[1].classList.toggle(`colored--dark`); //* Solved by putting it in the bottom */
 };
-
-//! Automode: start at 1PM
+//? Automode: start at 1PM
 const autoMode = () => {
 	//? Turn on dark mode at 1PM (13:00), & turn off dark mode toggle button
 	timeOfDay >= 13
