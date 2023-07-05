8cb67f8ca41c5ee37f40f7ae53b8945f2c42f3ca
devCKVargas <50130192+devCKVargas@users.noreply.github.com>
2023-07-05 17:27:31 +0800

    Add: script to calculate day spent Fix: link-info popup position

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,22 @@ for (const link of links) {
 	// link.insertAdjacentElement("afterend", linkInfo);
 	link.parentNode.insertBefore(linkInfo, link.nextSibling);
 }
+
+const dayCounter = () => {
+	const dayNumber = document.querySelector("#current-day");
+	// const dateStart = new Date(2023, 4, 6).getTime() / (1000 * 3600 * 24); //! May 6, 2023
+	const dateNow = new Date().getTime() / (1000 * 3600 * 24);
+	const dateComplete = new Date(2023, 7, 14).getTime() / (1000 * 3600 * 24); //! Supposedly finish date: August 14, 2023
+	const remainingDays = dateComplete - dateNow;
+	const spentDays = Math.ceil(100 - remainingDays);
+	let rest = 10; //TODO add rest day */
+	dayNumber.innerText = `Day ${spentDays - rest}`;
+};
+dayCounter();
+
+// const autoMode = () => {
+// 	const timeOfDay = new Date().getHours();
+// 	//! Turn on dark mode at 1PM (13:00), & turn off dark mode toggle button
+// 	timeOfDay >= 13 ? (darkMode(), (darkModeBtn.checked = false)) : (darkModeBtn.checked = true);
+// };
+// autoMode();
